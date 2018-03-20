5e611dc28f90f7c69bb307ae7716e2dfe9aea661
BobDeKort <dekort.bob101@gmail.com>
2018-03-19 17:29:40 -0700

    minor improvements

--- project/Senario1.py
+++ project/Senario1.py
@@ -11,9 +11,19 @@ def get_routes_data():
     f.close()
     return dictionary
 
-def check_single_phone_number_cost(phone_number):
+def get_phone_numbers():
+    '''Open the given phonenumber file and creates a list with all the numbers'''
+    f = open(PHONE_NUMBERS_FILE)
+    array = list()
+    for line in f.readlines():
+        array.append(line.strip())
+    f.close()
+    return array
+
+def check_single_phone_number_cost(routes_data, phone_number):
     '''Returns the cost of a given phone number'''
-    routes_data = get_routes_data()
+    if routes_data is None:
+        routes_data = get_routes_data()
 
     state_code = phone_number[0:5]
     area_code = phone_number[5:8]
@@ -26,18 +36,13 @@ def check_single_phone_number_cost(phone_number):
     else:
         return 0
 
-def get_phone_numbers():
-    '''Open the given phonenumber file and creates a list with all the numbers'''
-    f = open(PHONE_NUMBERS_FILE)
-    array = list()
-    for line in f.readlines():
-        array.append(line.strip())
-    f.close()
-    return array
+def check_multiple_numbers(routes_data, phone_numbers):
+    print("multiple")
 
 def main():
     # Senario 1
-    print(check_single_phone_number_cost('+15124156620'))
+    routes_data = get_routes_data()
+    print(check_single_phone_number_cost(routes_data, '+15124156620'))
 
 if __name__ == '__main__':
     main()
